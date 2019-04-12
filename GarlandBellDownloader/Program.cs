ca21ca26e2653ef185e8aef60a8ac208adb15f3b
Nicholas Martini <nicho.martini@gmail.com>
2019-04-12 17:02:11 +0200

    Added list with each unique icon ID.

--- GarlandBellDownloader/Program.cs
+++ GarlandBellDownloader/Program.cs
@@ -14,7 +14,27 @@ namespace GarlandBellDownloader
     {
         static void Main(string[] args)
         {
+            //Gets a list of nodes from the local json database
+            List<Nodes.Node> nodes = new List<Nodes.Node>();
+            nodes = Nodes.GetNodes();
 
+            //Creates a list for storing icon IDs
+            List<int> icons = new List<int>();
+            
+            //Add to the list each unique icon ID
+            foreach (var node in nodes)
+            {
+                foreach (var item in node.items)
+                {
+                    if (!icons.Contains(item.icon))
+                    {
+                        icons.Add(item.icon);
+                        Console.WriteLine("Added {0} to the list", item.icon);
+                    }
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
